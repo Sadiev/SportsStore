97fc62e74d245db7e2aeed81fb16dff234876f14
Shod <dilshod.sadiev@outlook.com>
2019-09-12 09:33:21 -0400

    Completing chapter 8

--- SportsStore/Models/ViewModels/PagingInfo.cs
+++ SportsStore/Models/ViewModels/PagingInfo.cs
@@ -9,7 +9,7 @@ namespace SportsStore.Models.ViewModels
     {
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
-        public int CurrentPage { get; set; }
+        public int CurrentPage { get; set; } = 1;
         public int TotalPages =>
             (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
